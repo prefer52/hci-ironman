7d738a793bdd8f2b02cf39d64752f1cf252c427a
LeeHZ <126967574+asd56780@users.noreply.github.com>
2023-06-03 15:07:02 +0900

    Feat : 박스 위치 조정, 박스 벗어나면 발생하던 에러 예외처리

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ cap.set(3,wCam) # 3은 width
 cap.set(4,hCam) # 4는 height
 pTime = 0
 #cTime = 0
+frameR = 100
 
 # maxhands는 인식할 수 있는 손의 개수
 # detectionicon은 노이즈에도 상관없이 얼마나 손을 잘 인식하나
@@ -172,27 +173,40 @@ while True:
         active = 1
         #print(mode)
         putText(mode)
-        cv2.rectangle(img, (110, 20), (620, 350), (255, 255, 255), 3)
+        # 커서 박스 크기, 위치 조정
+        cv2.rectangle(img, (frameR, frameR - 50), (wCam - frameR, hCam - frameR - 50), (255, 255, 255), 3)
 
         if fingers[1:] == [0,0,0,0]: #thumb excluded
             active = 0
             mode = 'N'
             print(mode)
-        else:
+        else:  # 박스 끝으로 손가락이 움직이면 화면 끝으로 움직이도록 수정함
             if len(lmList) != 0:
                 x1, y1 = lmList[8][1], lmList[8][2]
                 w, h = autopy.screen.size()
-                X = int(np.interp(x1, [110, 620], [0, w - 1]))
-                Y = int(np.interp(y1, [20, 350], [0, h - 1]))
+                X = int(np.interp(x1, [frameR, wCam - frameR], [0, w]))
+                Y = int(np.interp(y1, [frameR - 50, hCam - frameR - 50], [0, h]))
                 cv2.circle(img, (lmList[8][1], lmList[8][2]), 7, (255, 255, 255), cv2.FILLED)
-                cv2.circle(img, (lmList[4][1], lmList[4][2]), 10, (0, 255, 0), cv2.FILLED)  #thumb
+                cv2.circle(img, (lmList[4][1], lmList[4][2]), 10, (0, 255, 0), cv2.FILLED)  # thumb
 
-                if X%2 !=0:
-                    X = X - X%2
-                if Y%2 !=0:
-                    Y = Y - Y%2
-                print(X,Y)
-                autopy.mouse.move(X,Y)
+                # cv2.circle(img, (lmList[20][1], lmList[20][2]), 10, (0, 255, 0), cv2.FILLED) #새끼손가락
+
+                if X % 2 != 0:
+                    X = X - X % 2
+                if Y % 2 != 0:
+                    Y = Y - Y % 2
+                print(w - X, Y)
+                # autopy.mouse.move(w-X,Y)
+                # 손가락이 박스 바깥으로 나가면 발생하던 out of range 에러 예외처리
+                try:
+                    autopy.mouse.move(X, Y)
+                except:
+                    if X > w - 1 and Y > h - 1:
+                        autopy.mouse.move(w - 1, h - 1)
+                    elif X > w - 1:
+                        autopy.mouse.move(w - 1, Y)
+                    elif Y > h - 1:
+                        autopy.mouse.move(X, h - 1)
                 
                 #  pyautogui.moveTo(X,Y)
                 if fingers[0] == 0:
